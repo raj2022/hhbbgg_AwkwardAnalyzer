17415645ce81976b74088590b568dc4249db384c
raj2022 <shivam.raj@niser.ac.in>
2026-01-27 19:52:25 +0100

    updated plotter for 2023 and 2022

--- hhbbgg_Plotter.py
+++ hhbbgg_Plotter.py
@@ -987,7 +987,7 @@ def stack1d_histograms(up, output_dir, blind=True):
                 "TTGG", 
                 "TTG"]
     signal_bases = [
-                    "NMSSM_X400_Y100",
+                    # "NMSSM_X400_Y100",
                     # "NMSSM_X400_Y125",
                     # "NMSSM_X400_Y150",
                     # "NMSSM_X500_Y100",
@@ -1086,7 +1086,7 @@ def stack1d_histograms(up, output_dir, blind=True):
         print(f"[OK] {region}/{var}")
 
 def main():
-    root_path = "/afs/cern.ch/user/s/sraj/Analysis/hhbbgg_AwkwardAnalyzer/outputfiles/merged/2023_All/hhbbgg_analyzer-v2-histograms.root" # plotting for 2022 All era
+    root_path = "/afs/cern.ch/user/s/sraj/Analysis/hhbbgg_AwkwardAnalyzer/outputfiles/merged/2022_All/hhbbgg_analyzer-v2-histograms.root" # plotting for 2022 All era
     up = uproot.open(root_path)
     out = "stack_plots"
     os.makedirs(out, exist_ok=True)
